2cfcbb44f9918a46a21e7ddffec6888f434ad3c1
Marc Williams <oti_instructor@hotmail.com>
2018-05-08 07:55:55 -0300

    Scaffolded Courses

---
+++ ContosoU2018/Controllers/CourseController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoU2018.Data;
+using ContosoU2018.Models;
+
+namespace ContosoU2018.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CourseController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+
+        //mwilliams:  Reading related data
+        //Build a custom method to return a sorted list of departments
+        //for our dropdown filter
+        private IQueryable<Course>GetCourses(int? SelectedDepartment)
+        {
+            //Get all departments sorted by name
+            var departments = _context.Departments.OrderBy(d => d.Name).ToList();
+
+            //Add ViewData for sharing data between controller and view
+            ViewData["SelectedDepartment"] =
+                new SelectList(departments, "DepartmentID", "Name", SelectedDepartment);
+
+            //Retrieve the parameter value from SelectedDepartment
+            int departmentID = SelectedDepartment.GetValueOrDefault();
+
+            //Get Courses belonging to that department
+            IQueryable<Course> Courses = _context.Courses
+                //if no department has been selected that means all courses
+                //otherwise just courses for that selected department
+                .Where(c => !SelectedDepartment.HasValue || c.DepartmentID == departmentID)
+                .OrderBy(d => d.CourseID)//order by
+                .Include(d => d.Department);//also bring the related departments
+
+            return Courses;
+
+        }
+
+        // GET: Course
+        //public async Task<IActionResult> Index()
+        //{
+        //    var schoolContext = _context.Courses.Include(c => c.Department);
+        //    return View(await schoolContext.ToListAsync());
+        //}
+
+        public async Task<IActionResult> Index(int? SelectedDepartment)
+        {
+            //The SelectedDepartment refers to a Select Box (dropdown) within view
+            IQueryable<Course> courses = GetCourses(SelectedDepartment);
+            return View(await courses.ToListAsync());
+        }
+
+        // GET: Course/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Department)
+                .SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Course/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name");
+            return View();
+        }
+
+        // POST: Course/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseID,Title,Credits,DepartmentID")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+        // GET: Course/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+        // POST: Course/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CourseID,Title,Credits,DepartmentID")] Course course)
+        {
+            if (id != course.CourseID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.CourseID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+        // GET: Course/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Department)
+                .SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseID == id);
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.CourseID == id);
+        }
+    }
+}
